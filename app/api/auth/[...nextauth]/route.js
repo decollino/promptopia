46428e65a177e3f252aa17696252d891dbd59058
bernardesTonnie <andre.bernardes@tonnietalent.com>
2023-07-12 15:11:54 -0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          const cleanedName = profile.name
+            .replace(/[^a-zA-Z0-9\s]/g, "") // Remove special characters
+            .replace(/\s/g, "") // Remove spaces
+            .toLowerCase(); // Convert to lowercase
+
+          await User.create({
+            email: profile.email,
+            username: cleanedName,
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
